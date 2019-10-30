03f77bc668f3c74e33774916b4aa84438670ba3b
Kimleng <kimlenghor2006@gmail.com>
2019-10-30 12:40:12 -0700

    make some changes

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,11 @@ import './App.css';
 import Login from './index';
 
 class App extends Component {
-  state = { users: [] }
-
-  componentDidMount() {
-    // Retrieves data from /users in the backend
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState( { users }));
-  }
 
   render() {
     return (
       <div className="App">
         <Login/>
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>)}
-        </ul>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+import Snackbar from '@material-ui/core/Snackbar';
 
 const styles = makeStyles(theme => ({
     root: {
@@ -40,10 +41,18 @@ const styles = makeStyles(theme => ({
 
 export default function Login() {
 
+    //error slide up
+    const [open, setOpen] = React.useState(false);
+    const [transition, setTransition] = React.useState(undefined);
+
+
     const state = {
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
+        firstname: "",
+        lastname: "",
+        isLogin: true
     };
 
     const updateEmail = e => {
@@ -58,57 +67,132 @@ export default function Login() {
         state.confirmPassword = e.target.value
     }
 
-    const test = () => {
-        console.log(state.email)
-        console.log(state.password)
-        console.log(state.confirmPassword)
+    const updateFirstname = e => {
+        state.firstname = e.target.value
+    }
+
+    const updateLastname = e => {
+        state.lastname = e.target.value
+    }
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleSubmit = () => {
+        //distinguish between login and signup
+        console.log(state.isLogin)
+        //Login
+        if (state.isLogin) {
+            if (document.getElementById("standard-username").value === "" || document.getElementById("standard-password").value === "") {
+                //setTransition();
+                //setOpen(true);
+                console.log("Please fill in all requirements!!!")
+            } else {
+                //login successfully
+            }
+        } else {
+            //Sign up
+            if (document.getElementById("standard-username").value === "" || 
+                document.getElementById("standard-password").value === "" ||
+                document.getElementById("firstname").value === "" ||
+                document.getElementById("lastname").value === "" ||
+                document.getElementById("confirmPassword").value === "") {
+                //setTransition();
+                //setOpen(true);
+                console.log("Please fill in all requirements!!!")
+            } else {
+                //sign up successfully
+            }
+        }
     }
 
     const switchToSignupPage = () => {
         document.getElementById("submitButton").innerHTML = "Sign up"
-        document.getElementById("welcome").innerHTML = "Glad to have you here!"
+        document.getElementById("welcome").innerHTML = "Sign up"
         document.getElementById("forgotLink").style.display = "none";
         document.getElementById("createAccountLink").style.display = "none";
         document.getElementById("backLink").style.display = "block";
         document.getElementById("confirmTextField").style.display = "block";
+        document.getElementById("firstTextField").style.display = "block";
+        document.getElementById("lastTextField").style.display = "block";
+        state.isLogin = false
     }
 
     const switchToLoginPage = () => {
         document.getElementById("submitButton").innerHTML = "Login"
-        document.getElementById("welcome").innerHTML = "Welcome back!"
+        document.getElementById("welcome").innerHTML = "Login"
         document.getElementById("forgotLink").style.display = "block";
         document.getElementById("createAccountLink").style.display = "block";
         document.getElementById("backLink").style.display = "none";
         document.getElementById("confirmTextField").style.display = "none";
+        document.getElementById("firstTextField").style.display = "none";
+        document.getElementById("lastTextField").style.display = "none";
+        state.isLogin = true
     }
 
     const classes = styles();
     return (
         <div className="Index">
+
+            <Snackbar
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={transition}
+                ContentProps={{
+                'aria-describedby': 'message-id',
+                }}
+                message={<span id="message-id">Please fill in all the requirements</span>}
+            />
+
             <Grid container className={classes.root}>
                 <Grid item xs={false} sm={4} md={7} className={classes.image} />  
                 <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
                     <div className={classes.paper}>
                         <form className={classes.form} noValidate id="submitForm">
-                            <label id="welcome">Welcome back!</label>
-                            
-                            <TextField
-                                id="standard-username"
-                                label="Username"
-                                margin="normal"
-                                autoFocus
-                                fullWidth
-                                onChange={updateEmail}
-                            />
+                            <label id="welcome">Login</label>
 
-                            <TextField
-                                id="standard-password"
-                                label="Password"
-                                type="password"
-                                margin="normal"
-                                fullWidth
-                                onChange={updatePassword}
-                            />
+                            <div className={"firstnameTextField"} noValidate id="firstTextField">
+                                <TextField
+                                    id="firstname"
+                                    label="First name"
+                                    margin="normal"
+                                    fullWidth
+                                    onChange={updateFirstname}
+                                />
+                            </div>
+
+                            <div className={"lastnameTextField"} noValidate id="lastTextField">
+                                <TextField
+                                    id="lastname"
+                                    label="Last name"
+                                    margin="normal"
+                                    fullWidth
+                                    onChange={updateLastname}
+                                />
+                            </div>
+
+                            <div className={"emailTextField"} noValidate id="emailField">
+                                <TextField
+                                    id="standard-username"
+                                    label="Username"
+                                    margin="normal"
+                                    autoFocus
+                                    fullWidth
+                                    onChange={updateEmail}
+                                />
+                            </div>
+
+                            <div className={"passwordTextField"} noValidate id="passwordField">
+                                <TextField
+                                    id="standard-password"
+                                    label="Password"
+                                    type="password"
+                                    margin="normal"
+                                    fullWidth
+                                    onChange={updatePassword}
+                                />
+                            </div>
 
                             <div className={"thirdTextField"} noValidate id="confirmTextField">
                                 <TextField
@@ -126,7 +210,7 @@ export default function Login() {
                                     color="primary" 
                                     className={classes.button} 
                                     fullWidth 
-                                    onClick= {test}>
+                                    onClick= {handleSubmit}>
                                     Login
                             </Button>
 
