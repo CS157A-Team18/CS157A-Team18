5acc6aa749b4286ed29e581ea4270c2f0fd30435
Dominic Pham <36905285+dpham123@users.noreply.github.com>
2019-10-30 15:26:44 -0700

    [D-16] Write Query for Signup Function

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,46 @@
 var express = require('express');
 var db = require('../database/dbQuery.js');
 var router = express.Router();
-const util = require('util');
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  /* Code for testing getUserCredentials() function. Replace 'bun' with any username
-  *  in the DB and verify that the response is as expected
-  */
-  db.getUserCredentials('bun').then(rows => {
-    if (rows.length == 0) {
-      res.sendStatus(401)
-      return
-    }
-
-    res.send(util.format('Password: %s', rows[0]['password']))
-  })
+  // Test case for existing user. In this case, the MySQL will return a 'ER_DUP_ENTRY'
+  // error since the username already exists and username is a primary key
+  const existingUsername = 'bun'
+  var testJSON = []
+  db.addUser(existingUsername, 'test', 'test', 'password').catch(err => {
+    testJSON.push({
+      "test_case": "existing_user",
+      "username": existingUsername,
+      "expected_output": "ER_DUP_ENTRY",
+      "actual_output": err.code
+    });
+  }).then(() => {
+    // Test case for non-existing user. In this case, no error is outputted if successful.
+    // Check DB to verify user has been created
+    const randomUsername = makeid(10)
+    db.addUser(randomUsername, 'test', 'test', 'password').then(() => {
+      testJSON.push({
+        "test_case": "new_user",
+        "username": randomUsername,
+        "expected_output": "",
+        "actual_output": ""
+      });
+      res.json(testJSON)
+    }).catch(err => {
+      if (err) console.log(err)
+    });
+  });
 });
 
 module.exports = router;
