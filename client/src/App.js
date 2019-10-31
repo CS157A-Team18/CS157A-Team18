b19b3364af01bbb17cfd303fc81c42443415c782
juliachin123 <juliachin8@gmail.com>
2019-10-31 11:40:44 -0700

    Added Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,13 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Users</h1>
+        <div class="tab">
+          <button class="tablinks" onclick="openCity(event, 'Home')">Home</button>
+        </div>
+        <div id="Home" class="tabcontent">
+          <h3>Discover recipes suitable for you.</h3>
+          <img src="images/salad.jpg" height="200" width="300"></img>
+        </div>
         <ul>
           {this.state.users.map(user =>
             <li key={user.id}>{user.username}</li>)}
