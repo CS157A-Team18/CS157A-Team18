e74cf60340ea9f53a1de05d3c93139e8a22c04e1
Kimleng <kimlenghor2006@gmail.com>
2019-12-02 22:30:43 -0800

    make the personal recipe clickable

--- client/src/PersonalRecipe.js
+++ client/src/PersonalRecipe.js
@@ -25,7 +25,7 @@ const tableIcons = {
   Add: forwardRef((props, ref) => <Add {...props} ref={ref} />),
   FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
   LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  chevron_right: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
 };
 
 // export default function PersonalRecipe() {
@@ -117,39 +117,3 @@ const tableIcons = {
   
 //}
 
-// class DetailPanelWithRowClick extends React.Component {
-//   render() {
-//     return (
-//       <MaterialTable
-//         columns={[
-//           { title: 'Name', field: 'name' },
-//           { title: 'Surname', field: 'surname' },
-//           { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-//           {
-//             title: 'Birth Place',
-//             field: 'birthCity',
-//             lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-//           },
-//         ]}
-//         data={[
-//           { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-//           { name: 'Zerya Betül', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-//         ]}
-//         title="Detail Panel With RowClick Preview"
-//         detailPanel={rowData => {
-//           return (
-//             <iframe
-//               width="100%"
-//               height="315"
-//               src="https://www.youtube.com/embed/C0DPdy98e4c"
-//               frameborder="0"
-//               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-//               allowfullscreen
-//             />
-//           )
-//         }}
-//         onRowClick={(event, rowData, togglePanel) => togglePanel()}
-//       />
-//     )
-//   }
-// }
