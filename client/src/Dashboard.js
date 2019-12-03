a0c6837582743c12d8b044b4c271d2461f26b8e4
Dominic Pham <36905285+dpham123@users.noreply.github.com>
2019-12-02 23:52:33 -0800

    [D-68] Connect Dashboard Backend with Frontend

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -29,6 +29,9 @@ import food3 from './images/food3.jpg';
 import food4 from './images/food4.jpeg';
 import food5 from './images/food5.jpg';
 import food6 from './images/food6.jpeg';
+import {getUID} from './firebase/firebaseAuth'
+
+const util = require('util');
 
 const styles = theme => ({
 
@@ -120,54 +123,67 @@ const styles = theme => ({
     },
 });
 
-const tileData = [
-    {
-        img: food1,
-        title: 'Polenta',
-        author: 'author',
-    },
-    {
-        img: food2,
-        title: 'Ossobuco',
-        author: 'author',
-    },
-    {
-        img: food4,
-        title: 'Risotto',
-        author: 'author',
-    },
-    {
-        img: food6,
-        title: 'Bottarga',
-        author: 'author',
-    },
-    {
-        img: food5,
-        title: 'Fettuccine Alfredo',
-        author: 'author',
-    },
-    {
-        img: food3,
-        title: 'Prohok',
-        author: 'author',
-    },
-];
-
-
 class Dashboard extends React.Component { 
     constructor(props) {
         super(props);
         
         this.state = {
+            tileData: [
+                {
+                    img_url: "https://static.vibe.com/files/2017/02/Roll-Safe-1485964928-compressed.jpg",
+                    name: 'Polenta',
+                },
+                {
+                    img_url: food2,
+                    name: 'Ossobuco',
+                },
+                {
+                    img_url: food4,
+                    name: 'Risotto',
+                },
+                {
+                    img_url: food6,
+                    name: 'Bottarga',
+                },
+                {
+                    img_url: food5,
+                    name: 'Fettuccine Alfredo',
+                },
+                {
+                    img_url: food3,
+                    name: 'Prohok',
+                },
+            ],
             anchorEl: null,
             setAnchorEl: null,
             mobileMoreAnchorEl: null,
-            setMobileMoreAnchorEl: null
+            setMobileMoreAnchorEl: null,
+            userFullName: ""
         }
     }
 
     handleCardClick = () => {
         window.location = '/recipe'
+    }
+    
+    componentDidMount() {
+        getUID().then(user => {
+            fetch(util.format('%s/api/dashboard?uid=%s', process.env.REACT_APP_EXPRESS_BACKEND, user.uid), {
+                method: "GET",
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(responseData => {
+                this.setState({
+                    userFullName: util.format('%s %s', responseData.firstName, responseData.lastName),
+                    tileData: responseData.allRecipes
+                })
+            })
+        })
     }
     
     render() {
@@ -294,9 +310,7 @@ class Dashboard extends React.Component {
                         </IconButton>
     
                         </div>
-    
-                        {/* <label id="name">Kimleng Hor</label> */}
-                        <Link to="/profile" id="name">Kimleng Hor</Link>
+                        <Link to="/profile" id="name">{this.state.userFullName}</Link>
                         <Link to="/login" id="name">Sign out</Link>
                         <div className={classes.sectionMobile}>
                             <IconButton
@@ -325,12 +339,12 @@ class Dashboard extends React.Component {
             <div className={classes.root} id = "trending">
                 <label id="category">Recommended</label>
                 <GridList className={classes.gridList} cols={2.5}>
-                    {tileData.map(tile => (
-                    <GridListTile key={tile.img}>
-                        <img src={tile.img} alt={tile.title} />
+                    {this.state.tileData.map(tile => (
+                    <GridListTile key={tile.img_url}>
+                        <img src={tile.img_url} alt={tile.name} />
                         <GridListTileBar
                             id = "gridlist"
-                            title={tile.title}
+                            title={tile.name}
                             classes={{
                             root: classes.titleBar,
                             title: classes.title,
@@ -353,182 +367,3 @@ class Dashboard extends React.Component {
 }
 
 export default withStyles(styles)(Dashboard)
-
-// export default function Dashboard() {
-//     const classes = useStyles();
-//     const [anchorEl, setAnchorEl] = React.useState(null);
-//     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
-
-//     const isMenuOpen = Boolean(anchorEl);
-//     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-
-//     const handleProfileMenuOpen = event => {
-//         //setAnchorEl(event.currentTarget);
-//     };
-
-//     const handleMobileMenuClose = () => {
-//         setMobileMoreAnchorEl(null);
-//     };
-
-//     const handleMenuClose = () => {
-//         setAnchorEl(null);
-//         handleMobileMenuClose();
-//     };
-
-//     const handleMobileMenuOpen = event => {
-//         setMobileMoreAnchorEl(event.currentTarget);
-//     };
-
-//     const menuId = 'primary-search-account-menu';
-//     const renderMenu = (
-//     <Menu
-//         anchorEl={anchorEl}
-//         anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//         id={menuId}
-//         keepMounted
-//         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//         open={isMenuOpen}
-//         onClose={handleMenuClose}
-//     >
-//     <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-//     <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-//     </Menu>
-//     );
-
-//     const mobileMenuId = 'primary-search-account-menu-mobile';
-//     const renderMobileMenu = (
-//     <Menu
-//         anchorEl={mobileMoreAnchorEl}
-//         anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-//         id={mobileMenuId}
-//         keepMounted
-//         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-//         open={isMobileMenuOpen}
-//         onClose={handleMobileMenuClose}
-//     >
-//     <MenuItem>
-//         <IconButton aria-label="show 4 new mails" color="inherit">
-//             <Badge badgeContent={4} color="secondary">
-//                 <MailIcon />
-//             </Badge>
-//         </IconButton>
-//         <p>Messages</p>
-//     </MenuItem>
-//     <MenuItem>
-//         <IconButton aria-label="show 11 new notifications" color="inherit">
-//             <Badge badgeContent={11} color="secondary">
-//                 <NotificationsIcon />
-//             </Badge>
-//         </IconButton>
-//         <p>Notifications</p>
-//     </MenuItem>
-//     <MenuItem onClick={handleProfileMenuOpen}>
-//         <IconButton
-//             aria-label="account of current user"
-//             aria-controls="primary-search-account-menu"
-//             aria-haspopup="true"
-//             color="inherit"
-//             >
-//             <AccountCircle />
-//         </IconButton>
-//     <p>Profile</p>
-//     </MenuItem>
-//     </Menu>
-//     );
-
-//     return (
-    // <div className = "main">
-    //     <div className={classes.grow}>
-    //         <AppBar position="static" id="appbar">
-    //             <Toolbar>
-    //                 <Typography className={classes.title} variant="h6" noWrap>
-    //                     Delight
-    //                 </Typography>
-    //                 {/* <Link to="/dashboard" id="menu-button">Home</Link> */}
-    //                 <Link to="/personalRecipe" id="menu-button">Recipes</Link>
-    //                 <Link to="/upload" id="menu-button">Upload</Link>
-                    
-    //                 {/* <Button id="menu-button">
-    //                     Marketplace
-    //                 </Button> */}
-
-    //                 <div className={classes.search}>
-    //                     <div className={classes.searchIcon}>
-    //                         <SearchIcon />
-    //                     </div>
-    //                     <InputBase
-    //                         placeholder="Search…"
-    //                         classes={{
-    //                         root: classes.inputRoot,
-    //                         input: classes.inputInput,
-    //                         }}
-    //                         inputProps={{ 'aria-label': 'search' }}
-    //                     />
-    //                 </div>
-    //                 <div className={classes.grow} />
-    //                 <div className={classes.sectionDesktop}>
-    //                     <IconButton
-    //                         edge="end"
-    //                         aria-label="account of current user"
-    //                         aria-controls={menuId}
-    //                         aria-haspopup="true"
-    //                         onClick={handleProfileMenuOpen}
-    //                         color="inherit"
-    //                     >
-    //                     <AccountCircle />
-    //                 </IconButton>
-
-    //                 </div>
-
-    //                 {/* <label id="name">Kimleng Hor</label> */}
-    //                 <Link to="/profile" id="name">Kimleng Hor</Link>
-    //                 <div className={classes.sectionMobile}>
-    //                     <IconButton
-    //                         aria-label="show more"
-    //                         aria-controls={mobileMenuId}
-    //                         aria-haspopup="true"
-    //                         onClick={handleMobileMenuOpen}
-    //                         color="inherit"
-    //                         >
-    //                         <MoreIcon />
-    //                     </IconButton>
-    //                 </div>
-    //             </Toolbar>
-    //         </AppBar>
-
-    //         {renderMobileMenu}
-    //         {renderMenu}
-    //     </div>
-
-    //     <div className = "titleView">
-    //         <label id="title">Eat fresh Live Healthy</label>
-    //             <br/>
-    //         <label id="subtitle">A Healthy Food For A Wealthy Mood!</label>
-    //     </div>
-
-    //     <div className={classes.root} id = "trending">
-    //         <label id="category">Recommended</label>
-    //         <GridList className={classes.gridList} cols={2.5}>
-    //             {tileData.map(tile => (
-    //             <GridListTile key={tile.img}>
-    //                 <img src={tile.img} alt={tile.title} />
-    //                 <GridListTileBar
-    //                     id = "gridlist"
-    //                     title={tile.title}
-    //                     classes={{
-    //                     root: classes.titleBar,
-    //                     title: classes.title,
-    //                 }}
-    //                 actionIcon={
-    //                     <IconButton aria-label={`star ${tile.title}`}>
-    //                         <StarBorderIcon className={classes.title} />
-    //                     </IconButton>
-    //                 }
-    //                 />
-    //             </GridListTile>
-    //             ))}
-    //         </GridList>
-    //     </div> 
-    // </div>
-    // );
-//}
